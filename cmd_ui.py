95bdab330f03a385114a909629562d34a3852ec8
iulian-alexandru04 <iulian_alexandru04@yahoo.com>
2020-06-27 19:38:00 +0300

    extract UI to separate file

---
+++ cmd_ui.py
@@ -0,0 +1,90 @@
+from collections import namedtuple
+from main import Song, Album, Artist
+
+
+def add_artist():
+    print('-----------Add-Artist----------')
+    print("artist's name: ", end='')
+    name = input()
+    print("artist's country: ", end='')
+    country = input()
+    sored_artists.append(Artist(name, country))
+
+
+def show_artist():
+    print('-----------Show-Artist----------')
+    print("artist's name: ", end='')
+    name = input()
+    for artist in sored_artists:
+        if artist.name == name:
+            print(artist)
+            return
+    print('Artist not found')
+
+
+def add_album_to_artist():
+    print('----------Add-Album----------')
+    print("artist's name: ", end='')
+    artist_name = input()
+    print("album's title: ", end='')
+    title = input()
+    print("album's year: ", end='')
+    year = input()
+    print("number of tracks: ", end='')
+    no_tracks = int(input())
+
+    album = Album(title, year)
+    for _ in range(no_tracks):
+        print('song title: ', end='')
+        album.add(Song(input()))
+    for artist in sored_artists:
+        if artist.name == artist_name:
+            artist.add_album(album);
+
+
+def add_song_to_album():
+    print('----------Add-Song----------')
+    print("artist's name: ", end='')
+    artist_name = input()
+    print("album's title: ", end='')
+    title = input()
+    for artist in sored_artists:
+        if artist.name == artist_name:
+            for album in artist.get_albums():
+                if album.title == title:
+                    print('song title: ', end='')
+                    album.add(Song(input()))
+
+
+def sort_disks_for_all_artists():
+    for artist in sored_artists:
+        artist.sort_albums_by_date()
+
+
+sored_artists = []
+def main():
+    Option = namedtuple('Option', 'handler, description')
+    options = [Option(add_artist, 'Add artist'),
+               Option(show_artist, 'Show artist'),
+               Option(add_album_to_artist, 'Add album for artist'),
+               Option(add_song_to_album, 'Add song to album'),
+               Option(sort_disks_for_all_artists, 'Sort all albums')]
+    no_options = len(options)
+    while True:
+        print('----------MainMenu----------')
+        for num, opt in enumerate(options):
+            print(f'{num + 1}.{opt.description}')
+        print(f'{no_options + 1}.Exit')
+        print('\nYour option: ', end='')
+
+        choise = int(input())
+        if choise <= no_options:
+            options[choise - 1].handler()
+        elif choise == no_options + 1:
+            return
+        else:
+            print('You have selected an invalid option!', 'Try again!', sep='\n')
+
+
+if __name__ == '__main__':
+    main()
